453b7307f0bc05d91fedebab8a35002ac7fdd52a
Heshan <heshan@app-monkeyz.com>
2015-12-04 15:54:38 +0530

    Init commit

---
+++ app/src/main/java/heshansandeepa/demo_coordinatelayout/TextInoutLayoutActivity.java
@@ -0,0 +1,19 @@
+package heshansandeepa.demo_coordinatelayout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class TextInoutLayoutActivity extends AppCompatActivity  {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_inout_layout);
+    }
+}
+
